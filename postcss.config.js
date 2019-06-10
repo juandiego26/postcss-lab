08c6f6e808f9a19aabc996a723cb509d29a6aa04
Juan Diego <silgajuandiego@gmail.com>
2019-06-10 10:53:32 -0500

    [Add] Add custom media queries

--- postcss.config.js
+++ postcss.config.js
@@ -3,10 +3,14 @@ module.exports = {
     // require('autoprefixer')({
     //   grid: true
     // }),
+    require('postcss-apply'),
     require('postcss-preset-env')({
+      stage: 1,
       autoprefixer: {
         grid: true,
-      }
+      },
+      preserve: true // Valor por defecto de "true".Se asemeja a customProperties
+      //cambiar a false para notar el cambio
     })
   ]
 }
